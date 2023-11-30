06d784af45b2370382474848d99cbc94ca27b4d4
Shaun Harrington <shaunharrington17@gmail.com>
2023-11-29 21:37:15 -0800

    Making everything flow together and not dropping info

--- Scripts/Cabo Game.R
+++ Scripts/Cabo Game.R
@@ -3,20 +3,7 @@
   
 }
 
-{
-  cards <- rep(
-    seq(0,13,1),
-    c(2, rep(4, 12), 2)
-  )
-  
-  actions <- c(
-    rep(NA, 7),
-    rep("peek", 2),
-    rep("spy", 2),
-    rep("swap", 2),
-    rep(NA, 2)
-  )
-}
+
 
 
 {
@@ -43,8 +30,6 @@
     
   }
   
-  
-  
   fn_beginTurn <- function(action, deck, discard){
     
     # Action is draw from deck or discard
@@ -56,48 +41,58 @@
     return(theReturn)
   }
   
-  fn_takeTurn <- function(hand, draw, move = NA, playAction = NA){
-    # move = 3
+  fn_takeTurn <- function(hand, draw, card_num = NA, playAction = NA){
+    # card_num = 3
     # hand = hands[[p]]
     # draw
+    
+    cat(paste("\n\nStart hand: ", paste(hand, collapse = ", ")))
     
     # Take card action
     if(!is.na(playAction)) fn_playCard()
     
     # Exchange move for card in hand location
-    if(!is.na(move)){
-      toDiscard <- hand[move]
-      hand[move] <- draw
+    if(!is.na(card_num)){
+      toDiscard <- hand[card_num]
+      hand[card_num] <- draw
     } else{
       toDiscard <- draw
     }
     
-    cat(paste("\n\nThe move: ", move))
+    
+    cat(paste("\nThe card_num: ", card_num))
     cat(paste("\nThe draw: ", draw))
     cat(paste("\nTo discard: ", toDiscard))
+    cat(paste("\nEnd hand: ", paste(hand, collapse = ", ")))
     
     discard <<- append(toDiscard, discard)
+    
+    list(hand, discard[1])
     
     return(hand)
     
   }
   
-  fn_turn <- function(players){
+  fn_turn <- function(hand, drawAction, playAction, card_num, deck, discard){
+    # hand:         Players cards
+    # drawAction:   Either 'fromDeck' or 'fromDiscard'
+    # playAction:   True or False on playing an action
+    # card_num:     Card location in hand to replace with draw
     
-    for(p in 1:players){
-      
-      draw <- fn_beginTurn(
-        action = sample(c("fromDeck", "fromDiscard"), 1), 
-        deck, discard[1]
-      )
-      hands[[p]] <<- fn_takeTurn(
-        hands[[p]],
-        draw, 
-        move = sample(c(1:4, NA), 1),
-        playAction = NA
-      )
-      
-    }
+    
+    draw <- fn_beginTurn(
+      action = drawAction,
+      deck, discard[1]
+    )
+    
+    hand <- fn_takeTurn(
+      hand = hand,
+      draw = draw, 
+      card_num = card_num,
+      playAction = NA
+    )
+    
+    return(hand)
   }
   
   fn_duplicateCards <- function(){}
@@ -136,37 +131,3 @@
   }
 }
 
-# Gameplay ----
-{
-  ## Setup ----
-  
-  
-  
-  # Start ----
-  {
-    deck <- cards
-    players = 2
-    
-    hands <- lapply(1:players, \(p){
-      fn_deal(deck, 4)
-    })
-    discard <- fn_deal(deck, 1)
-    
-    scores <- lapply(hands, sum)
-  }
-  
-  # Turn 1 ----
-  {
-    hands
-    fn_turn(players)
-    hands
-    
-    scores <- lapply(hands, sum)
-  }
-  
-  
-  length(deck)
-  
-  
-  hand <- p1_hand
-}
